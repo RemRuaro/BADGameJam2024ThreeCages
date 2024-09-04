e6620817c6aeb58016198ed2cb4677f883322ba5
Rem Ruaro <rem.r.ruaro@gmail.com>
2024-09-04 19:15:40 +0800

    Player Controls update_01
    [Player Controls] Added a method for the player character to instantiate projectiles

--- Assets/_Scripts/Player/PlayerMovementControlsFirstPerson.cs
+++ Assets/_Scripts/Player/PlayerMovementControlsFirstPerson.cs
@@ -18,6 +18,12 @@ public class PlayerMovementControlsFirstPerson : MonoBehaviour
     public LayerMask _WhatIsGround;
     bool isGrounded;
 
+    [Header("Shooting Controls")]
+    [SerializeField] Camera _PlayerCam;
+    [SerializeField] Transform _PlayerMuzzle;
+    int projectilesFired;
+    float counter;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -28,12 +34,27 @@ public class PlayerMovementControlsFirstPerson : MonoBehaviour
     {
         // Ground checker
         isGrounded = Physics.Raycast(transform.position, Vector3.down, _PlayerData._PlayerHeight * 0.5f + 0.2f, _WhatIsGround);
-        
+
         PlayerInput();
 
         // Drag handler
         if (isGrounded) playerRb.drag = _PlayerData._PlayerGroundDrag;
         else playerRb.drag = 0;
+
+        // Shooting controls
+        if (counter < 1 / _PlayerData._PlayerRof)
+        {
+            counter += Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                projectilesFired = 0;
+                FireProjectiles();
+                counter = 0;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -55,5 +76,32 @@ public class PlayerMovementControlsFirstPerson : MonoBehaviour
 
         // Add force
         playerRb.AddForce(movementDirection.normalized * _PlayerData._PlayerSpeed * 10.0f, ForceMode.Force);
+    }
+
+    private void FireProjectiles()
+    {
+        Ray ray = _PlayerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint = ray.GetPoint(5.0f);
+        if (Physics.Raycast(ray, out hit)) targetPoint = hit.point;
+
+        float x = Random.Range(-_PlayerData._ProjectileSpread, _PlayerData._ProjectileSpread);
+        float y = Random.Range(-_PlayerData._ProjectileSpread, _PlayerData._ProjectileSpread);
+        float z = Random.Range(-_PlayerData._ProjectileSpread, _PlayerData._ProjectileSpread);
+
+        Vector3 direction = targetPoint - _PlayerMuzzle.position;
+        Vector3 directionWithSpread = direction + new Vector3(x, y, z);
+
+        GameObject currentProjectile = Instantiate(_PlayerData._PlayerProjectile, _PlayerMuzzle.position, Quaternion.identity);
+        currentProjectile.transform.forward = directionWithSpread.normalized;
+        currentProjectile.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * _PlayerData._ProjectileForce, ForceMode.Impulse);
+
+        projectilesFired++;
+
+        if (projectilesFired < _PlayerData._ProjectileAmount)
+        {
+            Invoke(nameof(FireProjectiles), _PlayerData._TimeBetweenShots);
+        }
     }
 }
